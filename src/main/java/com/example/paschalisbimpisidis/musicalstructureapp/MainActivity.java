c6c87770c3f76d18860cd229b6a58da55b9d2522
paschalisbimpisidis <pbimpisidis@gmail.com>
2018-12-29 17:49:12 +0100

    added listener to Browse Songs button added app style

--- src/main/java/com/example/paschalisbimpisidis/musicalstructureapp/MainActivity.java
+++ src/main/java/com/example/paschalisbimpisidis/musicalstructureapp/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +15,22 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+        // Find the button that navigates to the song library
+        Button browse = findViewById(R.id.browse);
+
+        // Set a click listener on that View
+        browse.setOnClickListener(new OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent browseIntent = new Intent(MainActivity.this, SongLibrary.class);
+
+                // Start the new activity
+                startActivity(browseIntent);
+            }
+        });
 
 
     }
