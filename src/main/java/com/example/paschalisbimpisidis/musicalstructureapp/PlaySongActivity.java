bae8e95947000c9b7be01d8ea056761bd7048c6c
paschalisbimpisidis <pbimpisidis@gmail.com>
2018-12-30 13:20:54 +0100

    added intent for navigating back to Song Library

--- src/main/java/com/example/paschalisbimpisidis/musicalstructureapp/PlaySongActivity.java
+++ src/main/java/com/example/paschalisbimpisidis/musicalstructureapp/PlaySongActivity.java
@@ -3,6 +3,7 @@ package com.example.paschalisbimpisidis.musicalstructureapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class PlaySongActivity extends AppCompatActivity {
@@ -23,6 +24,22 @@ public class PlaySongActivity extends AppCompatActivity {
 
         TextView songArtistView = findViewById(R.id.playedSongArtist);
         songArtistView.setText(artist);
+
+        // Find the TextView that navigates to the song library
+        TextView back = findViewById(R.id.back);
+
+        // Set a click listener on that View
+        back.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the TextView is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link SongLibrary}
+                Intent backIntent = new Intent(PlaySongActivity.this, SongLibrary.class);
+
+                // Start the new activity
+                startActivity(backIntent);
+            }
+        });
     }
 
 }
